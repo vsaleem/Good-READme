e7768f36cd1523befb1edc9133ae2495b4bf12a3
vsaleem <vic.saleem@gmail.com>
2020-03-21 00:54:03 -0400

    Made changes to index.js file.

--- Develop/index.js
+++ Develop/index.js
@@ -1,29 +1,67 @@
-// Create a command-line application that dynamically generates a PDF profile from a GitHub username. 
-//The application will be invoked with the following command:
-
-//```sh
-// node index.js
-// ```
-
-// STEP 1: SET UP NODE MODULES
+// STEP 1: SET UP NODE MODULES LIBRARIES
 const fs = require('fs');
-// const inquirer = require('inquirer');
+const inquirer = require('inquirer');
 const path = require('path');
+const generateHTML = require('./generateHTML')
+const prompt = inquirer.createPromptModule();
+
+// STEP 1: The user will be prompted to enter GitHub Username and favorite color,
+// inquirer
+    // .prompt([
+    //     { type: 'input', 
+    //         message: 'What is your GitHub Username?', 
+    //         name: '',
+    //     },
+    //     { type: 'list', 
+    //         message: 'What is your favorite color?', 
+    //         name: '',
+    //         choices: [ /*Choices array or function returning a choices array*/],
+    //     }
+    // ])
+
+const questions = [
+    {   
+        type: 'input',
+        message: 'What is your GitHub Username?',
+        name: '',
+    },
+    {   
+        message: 'What is your favorite color?',
+    },
+];
+console.log(questions)
 
 
-// STEP 1: The user will be prompted to enter GitHub Username,
-// STEP 2: The user will be prompted to select a favorite color, 
+
+// STEP 2: The user's answers will activate generator, 
+    prompt(questions).then(inquirer => {
+        //User feedback
+    })
+    // console.log(answers);
+
+    .catch(err => {
+        if(err.isTtyError) {
+            // For some reason the prompt couldn't be rendered
+        } else {
+            // Something else went wrong
+        }
+    });
+
+
+
+
+
+
 // STEP 3: THE COLOR SELECTION will be used as the background color for cards.
 
 // STEP 4: CREATE AND WRITE TO FILE: A .PDF doc will Populate with the following GitHub Account info:
 //CALL DATA
-const generateHTML = require('./generateHTML')
 
 //CREATES AND WRITES TO PDF
-fs.writeFile(path.join(__dirname, './', 'UserProfile.pdf'),(generateHTML, data),err => {
-    if (err) throw err;
-    console.log('User Profile created!');
-});
+// fs.writeFile(path.join(__dirname, './', 'UserProfile.pdf'),(generateHTML, data),err => {
+//     if (err) throw err;
+//     console.log('User Profile created!');
+// });
 
 // console.log(generateHTML);
 
@@ -40,16 +78,7 @@ fs.writeFile(path.join(__dirname, './', 'UserProfile.pdf'),(generateHTML, data),
         // * Number of users following
 
 
-// const questions = [
-//     {   type: 'input',
-//         name: 'title',
-//         message: 'What is your GitHub Username?',
-//     },
-//     {   type: 'input',
-//         name: 'title',
-//         message: 'What is your favorite color?',
-//     },
-// ];
+
 
 // function writeToFile(fileName, data) {
  
