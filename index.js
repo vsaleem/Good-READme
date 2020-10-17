49766344996121fcbf5a6183d5f887136a845f8a
vsaleem <vic.saleem@gmail.com>
2020-10-16 21:38:41 -0500

    Updated psuedo code instructions.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// 8>8>8> The best way to learn is to teach it to someone else. <3<3<3
+
+// CREATE YOUR OWN README.md!
+// Create a simple node / express generator to make README.md files for your GitHub Repositories, professional resumes, and more.
+
+// Follow the steps below:
+
 // STEP 1: SET UP NODE MODULES LIBRARIES
 const fs = require('fs');
 const inquirer = require('inquirer');
@@ -6,22 +13,21 @@ const axios = require('axios');
 const util = require("util");
 // const writeFileAsync = util.promisify(fs.writeFile);
 
-///////////////////
+console.log('Ready to create your README.md file.');
 
-console.log('Lets make your resume!!');
-// STEP 2: The user will be prompted to answer the following questions
 
-function promptUser(){
+// STEP 2:  Create a function called promptUser(). Inquirer.js is used to prompt the user to answer the following array of questions.
+function promptUser() {
     return inquirer.prompt([
-        { 
-            type: 'input', 
-            message: 'What is your GitHub Username?', 
+        {
+            type: 'input',
+            message: 'What is your GitHub Username?',
             name: 'username'
         },
 
         {
             type: 'input',
-            message: 'What is your project title name?',
+            message: `What is your Repositories' title name?`,
             name: 'title'
         },
 
@@ -30,7 +36,7 @@ function promptUser(){
             message: 'Describe your project in 3 sentences or less.',
             name: 'description'
         },
-        
+
         {
             type: 'input',
             message: 'Do you want to include installation instructions? If so, please describe.',
@@ -44,24 +50,24 @@ function promptUser(){
         },
 
         {
-            type: 'list', 
-            message: 'Would you like to include a license for this project?', 
+            type: 'list',
+            message: 'Would you like to include a license for this project?',
             name: 'license',
             default: '',
-            choices: [ 'Apache License 2.0',
-                        'GNU General Public License v3.0',
-                        'MIT License',
-                        'Mozilla Public License 2.0',
-                        'None' ],  
+            choices: ['Apache License 2.0',
+                'GNU General Public License v3.0',
+                'MIT License',
+                'Mozilla Public License 2.0',
+                'None'],
         },
 
-        { 
-            type: 'list', 
-            message: 'What is your favorite color?', 
+        {
+            type: 'list',
+            message: 'What is your favorite color?',
             name: 'favColor',
             default: '#008f68',
-            choices: [ 'Red', 'Blue', 'Green', 'Pink'],
-        }, 
+            choices: ['Red', 'Blue', 'Green', 'Pink'],
+        },
 
         {
             type: 'input',
@@ -87,49 +93,35 @@ function promptUser(){
             name: 'questions'
         },
 
-    ]) 
-    };
-        
-    // console.log('Okkkkk! Time to generate your profile!')
-    // STEP 3: This function uses the answers from the user to activate the README Generator
-    function init(response){            
+    ])
+};
 
-            const url = `https://api.github.com/users/${response.username}`;
-            let answers = response
-        
-            axios.get(url).then(function(data){
-              let followers = data.data.followers;
-              let location = data.data.location;
-              let bioImg = data.data.avatar_url;
-              let readmeFile = generateMarkdown(answers, followers, location, bioImg)
-              fs.writeFile("README.md", readmeFile, function(error){
-                  if(error){
-                      throw error;
-                  }else{
-                      console.log("it worked!!")
-                  }
-              })
+// STEP 3: Create a function that uses the user's prompt responses to initiate the README Generator. Use Axios to retrieve API information.
+function init(response) {
+
+    const url = `https://api.github.com/users/${response.username}`;
+    let answers = response
+
+    axios.get(url).then(function (data) {
+        let followers = data.data.followers;
+        let location = data.data.location;
+        let bioImg = data.data.avatar_url;
+        let readmeFile = generateMarkdown(answers, followers, location, bioImg)
+        fs.writeFile("README.md", readmeFile, function (error) {
+            if (error) {
+                throw error;
+            } else {
+                console.log("You have successfully created a README.md!")
+            }
         })
-    };
-    promptUser().then((results)=>{
-        init(results);
-    });
+    })
+};
+
+// LAST STEP: Initiate promptUser function with .then response of results.
+promptUser().then((results) => {
+    init(results);
+});
 
 
-
-// NOTE TO SELF -LAST STEPS:
-    // Look at homework requirements +++++Write questions you need 
-    // Look at data that you get back from gitHub. Ex. Profile picture, etc.
-    // Structure readme correctly!!! Needs Table of Contents etc...
-    
-// BONUS: CREATE PROMPT TO CHANGE THE FONT AND COLOR OF THE README BY SELECTING FAVORITE COLOR.
-    // then(
-    // async function init() {
-
-    //     const answers = await favColor();
-    //     answers.favColor = answers.favColor.EventEmitter("");
-    //     return answers;
-    // })
-    //     console.log(answers);
-    // init();
-
+// October 16, 2020
+// NOTE: CREATE effect to change the font-color by using the VOID OPERATOR.
